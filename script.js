e3bf7e774ebd2b8d26d68496fe61f7d561da32c2
AliceElizabethCarrizo <carrizo.franco@tecnica7.edu.ar>
2024-07-10 03:34:19 -0300

    pagina todo pelota

---
+++ script.js
@@ -0,0 +1,9 @@
+         // Iniciar automáticamente la reproducción del primer video al cargar la página
+        document.addEventListener('DOMContentLoaded', function () {
+            var video1 = document.getElementById('video1');
+            var video2 = document.getElementById('video2');
+            var video3 = document.getElementById('video3');
+            video1.play(); 
+            video2.play();
+            video3.play();
+        });
