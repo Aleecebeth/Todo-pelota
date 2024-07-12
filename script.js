fad4e985309abffcec521b14e1244343c512c55b
AliceElizabethCarrizo <carrizo.franco@tecnica7.edu.ar>
2024-07-12 11:35:20 -0300

    version casi final

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
             var video1 = document.getElementById('video1');
             var video2 = document.getElementById('video2');
             var video3 = document.getElementById('video3');
+            var video4 = document.getElementById('video3');
             video1.play(); 
             video2.play();
             video3.play();
+            video4.play();
         });
